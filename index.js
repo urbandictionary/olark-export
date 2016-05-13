22c312ce01d1ac8f9ede708e13d5affe5d924c0e
Alex0007 <i@alex0007.ru>
2016-05-13 20:36:53 +0500

    iQIcBAABAgAGBQJXNfSWAAoJEPW5xATFD1IA6aIP/RP4jaCb2bhf8Uve+Hts3Cbe  RZK4XujNRfuGS0PZMNQa8z8LlZBh3Tf4ZYlpgjppZUh2SoAUZE59qsQE8/7yMKCf  h6azMF/3rSE/KSlfbx54hM4GvRjTgbXsBoV2CEWSarGvTxsZlQc0/uCH0KzxB0zK  KcQUcYf55lp5agIXDNDySbbrwiz26tlBtwqKlmN77tnW5RNpPVrzDEKXEsIM3yHv  tA4K690tY4izRzmb6xYSnHOXoV+DikiVABXjFJsWI9I3lKvRlk7FZ8MG/UYFqv3r  MbfrQsGLFFO5129HpBVv7EtWhoTjTIIDeVHNffZkFu3QEtr1TcHSwDuHYzp2A8Lq  wlurQbukCScMmmM6w7aPnpXoJGZGN1WLczLNDQ4gODBH01PIqMA9HeADA8AfoRk9  F0ME99masz3t5xdgGxVl3UwiXL1iPjSd0Cl0ky+XMHEKThPFTdD9XbW9DlGerOfr  S/sSB+hOw06uer3gNu5XJvZb4nUi0j9Zaxs43AOcyK2SYphG3QIhfnYKkLxe4DDg  XBtq7ks8ki/mSMNfg9OEiPRlD/20i/iYU4LMnEhcuqBFF3z7+kRUVQZrspPvJBPm  kmvECIC60294787Nr93axH8AxD84oeVF+5reU11+DWsL7TBcQiJqxtGmU8Os/LpI  td2fK2K9I1eXhuGJ9Yov  =ap/f  -----END PGP SIGNATURE-----
    init

---
+++ index.js
@@ -0,0 +1,86 @@
+import fs from 'fs'
+import Rx from 'rx'
+import request from 'request'
+import cheerio from 'cheerio'
+import cookieString from './olark-cookie'
+
+let completed = false
+let startFromPage = 0
+
+const requestOptions = {
+  timeout: 40000,
+  headers: {
+    Cookie: cookieString
+  }
+}
+const ITEMS_ON_PAGE = 30
+
+let parsePageWithLinks = (html) => {
+  let links = []
+  let $ = cheerio.load(html)
+
+  $('.transcripts-wrapper').find('a').each(function(index, item) {
+    console.log('found', item.attribs.href)
+    links.push(`https://www.olark.com/${item.attribs.href}`)
+  })
+
+  return links
+}
+
+let downloadPageWithLinks = (url) => new Promise((resolve, reject) => {
+  console.log(`Crawling page #${url}`)
+  request.get(url, requestOptions, (err, resp, body) => {
+    if (err) throw err
+    if (body.indexOf('class="transcript-not-found"') > -1) {
+      console.log('completed')
+      completed = true
+      return resolve(null)
+    }
+    resolve(body)
+  })
+})
+
+let downloadLink = (url) => new Promise((resolve, reject) => {
+  console.log('Getting', url)
+  request.get(url, requestOptions, (err, resp, body) => {
+    if (err) throw err
+    resolve({
+      html: body,
+      url: url
+    })
+  })
+})
+
+let parseLink = (obj) => {
+  let $ = cheerio.load(obj.html)
+
+  return {
+    url: obj.url,
+    data: `<div class="transcripts-module">${$('div.transcripts-module').html()}</div>`
+  }
+}
+
+let saveToFile = (obj) => {
+  let filename = `${obj.url.split('/').pop()}.html`
+  fs.writeFile(`transcripts/${filename}`, obj.data, () => {
+    console.log(`${filename} saved`)
+  })
+  return filename
+}
+
+Rx.Observable
+  .return(startFromPage)
+  .map(_ => `https://www.olark.com/transcripts/show?start_position=${startFromPage++ * ITEMS_ON_PAGE}`)
+  .flatMap(_ => Rx.Observable.defer(() => downloadPageWithLinks(_)))
+  .doWhile(_ => !completed)
+  .take(2)
+  .scan(_ => _)
+  .flatMap(_ => parsePageWithLinks(_))
+  .flatMapWithMaxConcurrent(10, _ => Rx.Observable.defer(() => downloadLink(_)))
+  .map(_ => parseLink(_))
+  .map(_ => saveToFile(_))
+  .subscribe((data) => {}, (err) => {
+    console.log('err', err)
+  }, () => {
+    console.log('completed')
+  })
